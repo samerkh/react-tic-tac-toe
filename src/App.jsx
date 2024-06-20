0ecaf8137a250f0979ae6c847331aff1ec0f9fc1
Samer Alkhatib <khatib@cotede.ps>
2024-06-20 21:10:55 +0300

    Create board and player state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard/GameBoard";
 import Player from "./components/Player/Player";
 
 function App() {
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setCurrentPlayer((player) => (player === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="player1" symbol="X" />
-          <Player initialName="player2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="player1"
+            symbol="X"
+            isActive={currentPlayer === "X"}
+          />
+          <Player
+            initialName="player2"
+            symbol="O"
+            isActive={currentPlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSquareClick={handleSelectSquare}
+          currentPlayer={currentPlayer}
+        />
       </div>
       LOG
     </main>
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,14 +1,29 @@
-const initialBoard = Array(3).fill(Array(3).fill(null));
+import { useState } from "react";
 
-export default function GameBoard() {
+export default function GameBoard({ onSquareClick, currentPlayer }) {
+  const [board, setBoard] = useState(Array(3).fill(Array(3).fill(null)));
+
+  function handleClick(i, j) {
+    if (board[i][j] !== null) {
+      return;
+    }
+    setBoard((prevBoard) => {
+      const newBoard = prevBoard.map((row) => [...row]);
+      newBoard[i][j] = currentPlayer;
+      return newBoard;
+    });
+
+    onSquareClick(i, j);
+  }
+
   return (
     <ol id="game-board">
-      {initialBoard.map((row, i) => (
+      {board.map((row, i) => (
         <li key={i}>
           <ol>
             {row.map((col, j) => (
               <li key={j}>
-                <button></button>
+                <button onClick={() => handleClick(i, j)}>{board[i][j]}</button>
               </li>
             ))}
           </ol>
